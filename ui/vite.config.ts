46fb891d4c603220c9761dc6343a88582ddc01c0
Alex Li <alex@solsticehealth.co>
2026-03-03 00:10:43 -0500

    edits to get chat to work

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,11 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
